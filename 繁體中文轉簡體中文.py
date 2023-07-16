1b9504abc319e87463631329ebad99a986353873
k25c2yf <k25c2yf@yahoo.com.hk>
2023-07-16 11:37:22 +0800

    Add files via upload

---
+++ 繁體中文轉簡體中文.py
@@ -0,0 +1,29 @@
+import os
+import opencc
+
+# 設定轉換器
+converter = opencc.OpenCC('t2s')  # 繁體中文轉簡體中文
+
+# 設定原始資料夾和目標資料夾路徑
+src_dir = './big5'
+dst_dir = './gbV1'
+
+# 檢查目標資料夾是否存在，如果不存在則建立目標資料夾
+if not os.path.exists(dst_dir):
+    os.makedirs(dst_dir)
+
+# 處理所有的*.txt檔案
+for file_name in os.listdir(src_dir):
+    if file_name.endswith('.txt'):
+        # 讀取原始檔案內容
+        src_path = os.path.join(src_dir, file_name)
+        with open(src_path, 'r', encoding='utf-16-le') as f:
+            content = f.read()
+
+        # 執行繁簡轉換
+        content = converter.convert(content)
+
+        # 寫入轉換後的檔案到目標資料夾
+        dst_path = os.path.join(dst_dir, file_name)
+        with open(dst_path, 'w', encoding='utf-16-le') as f:
+            f.write(content)
