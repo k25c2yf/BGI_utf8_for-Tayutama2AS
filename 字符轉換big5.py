fcf6a38724b1d0fa6809f8937d0ae23c1501d36c
k25c2yf <k25c2yf@yahoo.com.hk>
2023-09-24 00:02:21 +0800

    Add files via upload

---
+++ 字符轉換big5.py
@@ -0,0 +1,19 @@
+import sys
+
+def string_to_hex(input_str, encoding):
+    encoded_str = input_str.encode(encoding)
+    hex_str = encoded_str.hex().upper()
+    return hex_str
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python wordtohex.py <input_string>")
+        sys.exit(1)
+
+#Shift-JIS  big5 gbk
+    input_str = sys.argv[1]
+    utf8_hex = string_to_hex(input_str, "utf-8")
+    print(f'UTF8="{utf8_hex}"')
+    ansi_hex = string_to_hex(input_str, "big5")
+    print(f'ANSI="{ansi_hex}"')
+    
